02bffd98c1855a5c67e06ad4f19d7fdef16319e7
Pranay Joshi <35572191+pranayteaches@users.noreply.github.com>
2019-12-13 22:29:37 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-    <a href="http://localhost:8080/geoserver/wms/reflect?format=application/openlayers&layers=pranay:india_pranay">
-      <img src="http://localhost:8080/geoserver/wms/reflect?layers=pranay:india_pranay&width=400"/>
-      </a>
- </html>
